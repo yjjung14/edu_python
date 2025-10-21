c4694896dabc40d10b6c7531b7a0c7cf9e14879e
yjjung14 <yjjung14@gmail.com>
2025-10-21 18:14:53 +0900

    epsoid23 commit

---
+++ episode4.py
@@ -0,0 +1,34 @@
+#
+# # 정수
+# age = 50
+# print(age)
+# print(type(age))
+#
+# age_mon = 50.9
+# print(age_mon)
+# print(type(age_mon))
+#
+# greeting = "Hello"
+# print(greeting)
+# print(type(greeting))
+#
+# is_adunt = True
+# print(is_adunt)
+# print(type(is_adunt))
+#
+# #--------------
+# fruits =[1,2,"사과", "바나나", "딸기", "메론"]
+# print(fruits)
+# print(type(fruits))
+#
+# tuple_mysize = ('168', '55', '75')
+# print(tuple_mysize)
+# print(type(tuple_mysize))
+
+student_score = {"영진":100, "영진":100, "승범":90, "연우":70}
+print(student_score)
+print(type(student_score))
+
+colors = {"red", "green", "blue","red", "blue", "yellow", "magenta"}
+print(colors)
+print(type(colors))
