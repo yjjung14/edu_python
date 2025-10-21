88a87608a113794543202dc9be6ade0f5a088f13
yjjung14 <yjjung14@gmail.com>
2025-10-21 18:29:12 +0900

    epsoid4 commit

---
+++ episode5.py
@@ -0,0 +1,23 @@
+
+# age = int(input("your age: "))
+#
+# if age >= 19:
+#     print(" 성인")
+# elif 16<= age <=18:
+#     print(" 고삐리")
+# elif 13 <= age <=15:
+#     print(" 초딩")
+# else:
+#     print(" 미 성년")
+#
+# print("BYE")
+
+# for num in range(1, 101):
+#     print(num)
+
+num = 1
+while num <=100:
+    print(num)
+    num = num + 1
+
+print("end")
