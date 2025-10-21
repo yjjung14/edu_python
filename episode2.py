acf2fe593496872fb6217a3bb510b823e2e37804
yjjung14 <yjjung14@gmail.com>
2025-10-21 18:03:21 +0900

    epsoid23 commit

---
+++ episode2.py
@@ -0,0 +1,5 @@
+name = input(" input your name : ")
+
+print(name)
+
+test
---
+++ episode3.py
@@ -0,0 +1,16 @@
+##range range func
+#for inum in range(1, 101):
+#     print(inum)
+# print("END")
+
+# list for문
+# fruits =["사과", "바나나", "딸기", "메론"]
+#
+# for i in fruits:
+#     print(i)
+
+# dictionary for문
+student_score = {"영진":100, "승범":90, "연우":70}
+for name, score in student_score.items():
+    print(name, score)
+print("END")
